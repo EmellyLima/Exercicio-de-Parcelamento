2727074482431cc52df16ad83c63020c6d2ab823
Emelly lima <111100167+EmellyLima@users.noreply.github.com>
2022-09-02 13:36:00 -0300

    Update Contract.cs

--- Parcelamento/Entities/Contract.cs
+++ Parcelamento/Entities/Contract.cs
@@ -9,12 +9,19 @@ namespace Entities
         public int Number { get; set; }
         public DateTime Date { get; set; }
         public double TotalValue { get; set; }
+        public List<Installment> installment {get; set: }
 
         public Contract(int number, DateTime date, double totalValue)
         {
             Number = number;
             Date = date;
             TotalValue = TotalValue;
+            installment = New List<Installment>();
+        }
+        
+        public void AddInstallment(Installment installment)
+        {
+            installments.Add(installment)
         }
     }
 }
